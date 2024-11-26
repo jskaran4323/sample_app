14079450a12f52b8bb862b59fabdbfca1437c73e
jskaran4323 <jskaran782742@gmail.com>
2024-11-26 17:59:33 -0500

    login error solved, implemented editing, showing, deleteing users

--- lib/sample_app_web/router.ex
+++ lib/sample_app_web/router.ex
@@ -27,8 +27,10 @@ defmodule SampleAppWeb.Router do
     get("/signup", UserController,:new, as: :signup)
     post("/signup", UserController,:create, as: :signup)
     get("/login", SessionController, :new,as: :login)
+
     post("/login", SessionController, :create,as: :login)
-    delete("/logout", SessionController, :delete ,as: :logout)
+
+    get "/logout", SessionController, :delete
 
     resources "/users", UserController, only: [:index, :show, :edit, :update, :delete]
   end
